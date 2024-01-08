adee0495582eaf93e7e648443a8a868285a329d5
PamGold <badtrailz@gmail.com>
2024-01-08 21:21:31 +0100

    Added Health.jsx, Science.jsx, Sports.jsx, Technology.jsx and Edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,35 @@ import "./App.css";
 
 import Layout from "./layout/Layout";
 
-
 // Pages
-
 
 import Home from "./pages/Home";
 import Business from "./pages/Business";
 import Entertainment from "./pages/Entertainment";
+import Health from "./pages/Health";
+import Science from "./pages/Science";
+import Sports from "./pages/Sports";
 import Search from "./pages/Search";
+import Technology from "./pages/Technology";
+
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-<Route index element={<Home/>}/>
-<Route path="business" element={<Business/>}/>
-<Route path="entertainment" element={<Entertainment/>}/>
-<Route path="search" element={<Search/>}/>
-<Route/>
-<Route/>
-<Route/>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="business" element={<Business />} />
+      <Route path="entertainment" element={<Entertainment />} />
+      <Route path="search" element={<Search />} />
+      <Route path="health" element={<Health />} />
+      <Route path="science" element={<Science />} />
+      <Route path="sports" element={<Sports />} />
+      <Route path="technology" element={<Technology />} />
     </Route>
   )
-)
+);
 
 function App() {
-  return  <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Health.jsx
@@ -0,0 +1,28 @@
+
+
+import { Grid } from "@chakra-ui/react";
+import { useEffect } from "react";
+import Article from "../components/Article";
+import '../App.css'
+import useStore from "../Store";
+
+const Health = () => {
+  const { data, fetchData } = useStore();
+  useEffect(() => {
+    fetchData("https://newsdata.io/api/1/news?apikey=pub_36099d663cea1cc3aa5d4b66cd157be2c5361&q=health");
+  }, []);
+
+  console.log(data);
+
+  // const news = data.map(item=>(<Article article={item}/>))
+
+  return (
+    <Grid className="news-grid" gridTemplateColumns={{base:"1fr", sm:"1fr", md:"1fr 1fr", lg:"1fr 1fr 1fr", xl:"1fr 1fr 1fr" }} gridTemplateRows={{base:"400px", sm:"400px", md:"400px", lg:"500px", xl:"500px"}} gap="25px">
+      {data.map((item) => (
+        <Article item={item} />
+      ))}
+    </Grid>
+  );
+};
+
+export default Health;
