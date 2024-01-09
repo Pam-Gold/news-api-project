742501cb35f80cf6e62e1330ed483c8b9a8b1b4d
PamGold <badtrailz@gmail.com>
2024-01-09 19:34:19 +0100

    Update Article.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,7 +5,7 @@ const Article = ({item}) => {
     <Box h="fit-content" p="10px">
 <Flex  flexDirection="column">
 
-<Img h={{base:"300px", sm:"300px", md:"300px", lg:"400px", xl:"400px"}} w="100%" src={item.image} backgroundRepeat="no-repeat"/>
+<Img h={{base:"300px", sm:"300px", md:"300px", lg:"400px", xl:"400px"}} w="100%" src={item.image} backgroundRepeat="no-repeat" borderRadius="8px"/>
        <Text mb="5px" fontWeight="600">{ item.title }</Text>
        <Text mb="15px">{item.source.name}</Text>
        <Link href={item.url} color="grey">Read More...</Link>
