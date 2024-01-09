1e6221ee8d2f7cada322d65e4820dfd95874388d
PamGold <badtrailz@gmail.com>
2024-01-09 19:27:20 +0100

    Update Article.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import {Flex, Text, Box, Img, Link} from '@chakra-ui/react';
 import '../App.css';
 const Article = ({item}) => {
   return(
-    <Box p="10px">
+    <Box h="fit-content" p="10px">
 <Flex  flexDirection="column">
 
 <Img h={{base:"300px", sm:"300px", md:"300px", lg:"400px", xl:"400px"}} w="100%" src={item.image} backgroundRepeat="no-repeat"/>
